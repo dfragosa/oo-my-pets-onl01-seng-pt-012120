ffa9376cd6bfb65499a2a846e006e68aa5c1a9a8
Derric Fragosa <fderric@gmail.com>
2020-02-19 17:13:04 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,15 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = mood
+  end
+  
+  def mood
+    feeling = "nervous"
+    feeling
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,29 @@
+require 'pry'
 class Owner
-  # code goes here
+   attr_reader :name, :species
+   
+   @@all = []
+ 
+  def initialize(name, species= "human")
+    @name = name
+    @species = species
+    @@all << self
+  end
+  
+  def say_species
+   phrase = "I am a human."
+   phrase
+  end
+  
+  def self.all
+   @@all
+  end
+  
+  def self.count
+    self.all.count
+  end
+  
+  def self.reset_all
+    self.all.clear
+  end
 end
